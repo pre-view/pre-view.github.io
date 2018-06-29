0fb3cb7e4748cd8bd28cb77d9b91fcf56aa26221
李昕亮 <lixinliang@yy.com>
2018-06-29 09:37:40 +0800

    update

--- make_readme_md.js
+++ make_readme_md.js
@@ -5,12 +5,15 @@ const fs = require('fs-extra');
 const path = require('path');
 const klawSync = require('klaw-sync');
 
+const git = 'https://github.com/pre-view/pre-view.github.io/tree/master';
+
 const template = [
     '[![Twitter](https://img.shields.io/badge/twitter-@qq393464140-blue.svg)](http://twitter.com/qq393464140)',
     '[![](https://img.shields.io/badge/made%20with-%E2%9D%A4-ff69b4.svg)](https://preview.lixinliang.com)',
     '',
     '# pre-view.github.io',
     'https://pre-view.github.io',
+    '',
 ];
 
 const list = klawSync(__dirname, {
@@ -20,53 +23,45 @@ const list = klawSync(__dirname, {
 _(list)
 .map(({ path }) => path)
 .filter(( dirname ) => /^\d+$/.test(path.basename(dirname)))
+.sortBy(( dirname ) => -path.basename(dirname))
 .forEach(( dirname ) => {
-    const list = klawSync(dirname, {
-        depthLimit: 1,
-    });
 
-    const map = {};
+    const year = path.basename(dirname);
+    template.push(
+        `### ${ year }`,
+        '',
+    );
+
+    const list = klawSync(dirname, {
+        depthLimit: 0,
+    });
 
     _(list)
     .map(({ path }) => path)
-    .filter(( filename ) => /^[a-zA-Z0-9\-]+\/(README\.md)|(index\.(html|pdf))$/.test(path.relative(dirname, filename)))
-    .forEach(( filename ) => {
-        const project = path.dirname(path.relative(dirname, filename));
-        map[project] = map[project] || [];
-        map[project].push(path.relative(__dirname, filename));
+    .filter(( dirname ) => /^[a-zA-Z0-9\-]+$/.test(path.basename(dirname)))
+    .forEach(( dirname ) => {
+
+        const presentation = path.basename(dirname);
+        template.push(presentation);
+
+        const list = klawSync(dirname, {
+            depthLimit: 0,
+        });
+
+        _(list)
+        .map(({ path }) => path)
+        .filter(( filename ) => /^(README\.md)|(index\.(html|pdf))+$/.test(path.relative(dirname, filename)))
+        .forEach(( filename ) => {
+
+            const file = path.basename(filename);
+            template.push(`* ${ git }/${ year }/${ presentation }/${ file }`);
+
+        });
+
+        template.push('');
+
     });
 
-    const year = path.basename(dirname);
-
-    console.log(year, map);
-
-    // path.relative(dirname, filename)
-
-    // fragment.push(
-    //     `* ${ path.relative(__dirname, filename) }`
-    // );
-
-    // if (fragment.length) {
-    //     [].push.apply(template, fragment);
-    // }
-
-
-    // if (!fragment.length) {
-    //     fragment.push(
-    //         '',
-    //         '---',
-    //         '',
-    //         `### ${ path.basename(dirname) }`,
-    //         '',
-    //     );
-    // }
-
 });
-
-template.push(
-    '',
-    '<!-- test -->',
-    '',
-);
 
 fs.outputFileSync(path.join(__dirname, './README.md'), template.join('\n'));
